35cc421a8a02e5dfd78ef742c84fb46e34a779cb
shortybig <jbrannock2018@gmail.com>
2025-01-25 15:59:16 -0500

    added magritter::%>% as a function and updated plot aes

--- treasuryBills.R
+++ treasuryBills.R
@@ -1,6 +1,9 @@
 loadNamespace("tidyverse")
 loadNamespace("httr")
 loadNamespace("gmailr")
+loadNamespace("magrittr")
+
+`%>%` <- magrittr::`%>%`
 
 # scrape from the website -------------------------------------------------
 
@@ -35,7 +38,7 @@ tbills_final <- tbills_int |>
   dplyr::mutate(
     dplyr::across(
       dplyr::matches("Date"),
-    ~ as.POSIXct(., format = "%Y-%m-%d", tz = "America/New_York"))
+      ~ as.POSIXct(., format = "%Y-%m-%d", tz = "America/New_York"))
   ) |> 
   dplyr::mutate(
     dplyr::across(
@@ -45,45 +48,65 @@ tbills_final <- tbills_int |>
   ) |> 
   dplyr::mutate(
     securityTerm = as.factor(securityTerm)
-    )
+  )
 
 # plots -------------------------------------------------------------------
 
+start_date <- "2022-01-01"
+
 tbills_final |> 
-    dplyr::filter(auctionDate > "2021-08-01") |> 
+  dplyr::filter(auctionDate >= start_date) %>% 
   
-  ggplot2::ggplot(ggplot2::aes(auctionDate, highInvestmentRate)) + 
-  ggplot2::geom_line(
-    ggplot2::aes(group = securityTerm, col = securityTerm),
-    linewidth = 1.8,
-    alpha = 0.6
-    ) + 
-  ggplot2::scale_color_discrete("Weeks", guide = ggplot2::guide_legend(override.aes = list(linewidth = 4, alpha = 1))) +
-  ggplot2::scale_x_datetime(date_breaks = "3 months", labels = function (x) format(x, "%Y-%m")) +
-  ggplot2::scale_y_continuous(breaks = seq(0, 7, by = 0.5)) +
-  ggplot2::theme(
-    panel.background = ggplot2::element_blank(),
-    panel.grid.major = ggplot2::element_line(color = "#cccccc"),
-    panel.grid.minor.y = ggplot2::element_line(color = "#cccccc"),
-    axis.ticks = ggplot2::element_blank(),
-    legend.position = "bottom",
-    legend.key = ggplot2::element_blank(),
-    legend.title = ggplot2::element_text(size = 18),
-    legend.text = ggplot2::element_text(size = 15),
-    legend.key.width = ggplot2::unit(2, "cm"),
-    axis.title = ggplot2::element_text(size = 20, hjust = 1),
-    axis.text.y = ggplot2::element_text(size = 18),
-    axis.text.x = ggplot2::element_text(size = 18, angle = 45, hjust = 1),
-    plot.title = ggplot2::element_text(size = 22, face = "bold"),
-    plot.subtitle = ggplot2::element_text(size = 20, face = "italic"),
-    text = ggplot2::element_text(family = "Arial Narrow")
-  ) + 
-  ggplot2::labs(
-    title = "Treasury Bills",
-    subtitle = "since AUG2021",
-    y = "Return of Investment (%)",
-    x = "Issue Date"
-  )
+  {
+    
+    ggplot2::ggplot(., ggplot2::aes(auctionDate, highInvestmentRate)) + 
+      ggplot2::geom_line(
+        ggplot2::aes(
+          group = securityTerm, 
+          col = securityTerm
+        ),
+        linewidth = 1.5,
+        alpha = 0.6
+      ) + 
+      ggplot2::scale_color_discrete(
+        "Weeks", 
+        guide = ggplot2::guide_legend(
+          override.aes = list(linewidth = 4, alpha = 1)
+        )
+      ) +
+      ggplot2::scale_x_datetime(
+        date_breaks = "3 months", 
+        labels = function (x) format(x, "%Y-%m")
+      ) +
+      ggplot2::scale_y_continuous(
+        breaks = seq(0, max(.$highInvestmentRate, na.rm = TRUE), by = 0.5)
+      ) +
+      ggplot2::theme(
+        panel.background = ggplot2::element_blank(),
+        panel.grid.major = ggplot2::element_line(color = "#cccccc"),
+        panel.grid.minor.y = ggplot2::element_line(color = "#cccccc", linetype = "dashed"),
+        panel.grid.major.x = ggplot2::element_line(linetype = c("solid", "dashed")),
+        axis.ticks = ggplot2::element_blank(),
+        legend.position = "bottom",
+        legend.key = ggplot2::element_blank(),
+        legend.title = ggplot2::element_text(size = 18),
+        legend.text = ggplot2::element_text(size = 15),
+        legend.key.width = ggplot2::unit(2, "cm"),
+        axis.title = ggplot2::element_text(size = 20, hjust = 1),
+        axis.text.y = ggplot2::element_text(size = 18),
+        axis.text.x = ggplot2::element_text(size = 18, angle = 45, hjust = 1),
+        plot.title = ggplot2::element_text(size = 22, face = "bold"),
+        plot.subtitle = ggplot2::element_text(size = 20, face = "italic"),
+        text = ggplot2::element_text(family = "Arial Narrow")
+      ) + 
+      ggplot2::labs(
+        title = "Treasury Bills",
+        subtitle = paste0("since ", start_date),
+        y = "Return of Investment (%)",
+        x = "Issue Date"
+      )
+    
+  }
 
 # notification ------------------------------------------------------------
 gmailr::gm_auth_configure()
